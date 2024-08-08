eccf82316d1eafd7eb0f8b1d31ad75cc2b0cd382
Amar_Asfaw <amarasfaw1077@gmail.com>
2024-08-08 22:00:15 +0300

    added different pages

---
+++ client/src/pages/Search.jsx
@@ -0,0 +1,147 @@
+import { Button, Select, TextInput } from 'flowbite-react';
+import { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import PostCard from '../components/PostCard';
+
+export default function Search() {
+  const [sidebarData, setSidebarData] = useState({
+    searchTerm: '',
+    category: 'uncategorized',
+    tag: '', // Add tag to sidebarData
+  });
+
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [showMore, setShowMore] = useState(false);
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    const categoryFromUrl = urlParams.get('category');
+    const tagFromUrl = urlParams.get('tag'); // Get tag from URL
+
+    if (searchTermFromUrl || categoryFromUrl || tagFromUrl) {
+      setSidebarData({
+        ...sidebarData,
+        searchTerm: searchTermFromUrl || sidebarData.searchTerm,
+        category: categoryFromUrl || sidebarData.category,
+        tag: tagFromUrl || sidebarData.tag,
+      });
+    }
+
+    const fetchPosts = async () => {
+      setLoading(true);
+      const searchQuery = urlParams.toString();
+      const res = await fetch(`/api/post/getposts?${searchQuery}`);
+      if (!res.ok) {
+        setLoading(false);
+        return;
+      }
+      const data = await res.json();
+      setPosts(data.posts);
+      setLoading(false);
+      setShowMore(data.posts.length === 9);
+    };
+    fetchPosts();
+  }, [location.search]);
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setSidebarData((prevData) => ({
+      ...prevData,
+      [id]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(sidebarData);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  const handleShowMore = async () => {
+    const numberOfPosts = posts.length;
+    const urlParams = new URLSearchParams(location.search);
+    urlParams.set('startIndex', numberOfPosts);
+    const searchQuery = urlParams.toString();
+    const res = await fetch(`/api/post/getposts?${searchQuery}`);
+    if (!res.ok) {
+      return;
+    }
+    const data = await res.json();
+    setPosts((prevPosts) => [...prevPosts, ...data.posts]);
+    setShowMore(data.posts.length === 9);
+  };
+
+  return (
+    <div className='flex flex-col md:flex-row'>
+      <div className='p-7 border-b md:border-r md:min-h-screen border-gray-500'>
+        <form className='flex flex-col gap-8' onSubmit={handleSubmit}>
+          <div className='flex items-center gap-2'>
+            <label className='whitespace-nowrap font-semibold'>
+              Search Term:
+            </label>
+            <TextInput
+              placeholder='Search...'
+              id='searchTerm'
+              type='text'
+              value={sidebarData.searchTerm}
+              onChange={handleChange}
+            />
+          </div>
+          <div className='flex items-center gap-2'>
+            <label className='font-semibold'>Category:</label>
+            <Select
+              onChange={handleChange}
+              value={sidebarData.category}
+              id='category'
+            >
+              <option value='uncategorized'>Uncategorized</option>
+              <option value='reactjs'>React.js</option>
+              <option value='nextjs'>Next.js</option>
+              <option value='javascript'>JavaScript</option>
+            </Select>
+          </div>
+          <div className='flex items-center gap-2'>
+            <label className='font-semibold'>Tag:</label>
+            <TextInput
+              placeholder='Tag...'
+              id='tag'
+              type='text'
+              value={sidebarData.tag}
+              onChange={handleChange}
+            />
+          </div>
+          <Button type='submit' outline gradientDuoTone='purpleToPink'>
+            Apply Filters
+          </Button>
+        </form>
+      </div>
+      <div className='w-full'>
+        <h1 className='text-3xl font-semibold sm:border-b border-gray-500 p-3 mt-5'>
+          Posts results:
+        </h1>
+        <div className='p-7 flex flex-wrap gap-4'>
+          {!loading && posts.length === 0 && (
+            <p className='text-xl text-gray-500'>No posts found.</p>
+          )}
+          {loading && <p className='text-xl text-gray-500'>Loading...</p>}
+          {!loading &&
+            posts.map((post) => <PostCard key={post._id} post={post} />)}
+          {showMore && (
+            <button
+              onClick={handleShowMore}
+              className='text-teal-500 text-lg hover:underline p-7 w-full'
+            >
+              Show More
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
